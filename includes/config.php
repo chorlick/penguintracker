024b0e72997a3afc1ac86d7d1c48bd1396371976
chorlick <chorlick@gmail.com>
2016-03-20 22:53:00 -0500

    Patched the paths for the proff.

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 
     //SET THIS TO THE DIRECTORY TO WRITE THE SQLITE DB TO 
-    $SQLITE_DB_LOCATION          = "C:\\Users\chorl_000\\Documents\\NetBeansProjects\\penguin\\db\\penguin.sqlite";
+    $SQLITE_DB_LOCATION          = "C:\\code\\frameworks\\xampp\\htdocs\\regis\\db\\penguin.sqlite";
     $SQLITE_CONNECTION_ARGS_ATTR = PDO::ATTR_ERRMODE;
     $SQLITE_CONNECTION_ARGS_EXCP = "PDO::ERRMODE_EXCEPTION";
     $CONFIG_CREATE_USER_TABLE    = true;
--- utils/start_web_server.bat
+++ utils/start_web_server.bat
@@ -4,6 +4,6 @@
 SET PHP_PATH=C:\Users\chorl_000\code\php\
 
 ::PATH TO TOP LEVEL SOURCE CODE DIRECTORY
-SET WEB_ROOT=C:\Users\chorl_000\Documents\NetBeansProjects\penguin
+SET WEB_ROOT=C:\code\frameworks\xampp\htdocs\regis
 
 %PHP_PATH%\php -S localhost:8000 -t %WEB_ROOT% -c %PHP_PATH%\php.ini
